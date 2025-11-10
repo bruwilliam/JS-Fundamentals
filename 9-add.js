5a4498d516fc27bc9e5572deae9046a186c97db2
bruwilliam <bruceopio@gmail.com>
2025-11-10 20:37:13 +0000

    Added a file 9-add.js

---
+++ 9-add.js
@@ -0,0 +1,13 @@
+
+function add(a, b) {
+    return a + b;
+}
+
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+
+if (isNaN(first) || isNaN(second)) {
+    console.log("NaN");
+} else {
+    console.log(add(first, second));
+}
