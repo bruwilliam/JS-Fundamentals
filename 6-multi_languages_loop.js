ab11ce8692c17edf1f9b4af21c740dea120c58e8
bruwilliam <bruceopio@gmail.com>
2025-11-10 18:54:27 +0000

    Added a file 6-multi_language_loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+
+const language_loop = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let index = 0; index < language_loop.length; index++) {
+    const element = language_loop[index];
+    console.log(element);
+}
